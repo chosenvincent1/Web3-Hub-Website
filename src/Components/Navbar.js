1fb406b1118b0c8bdd9b68f719fc888633b99a4d
Chosen Vincent <chosendev2000@gmail.com>
2022-12-16 14:06:02 +0100

    Getting Started

---
+++ src/Components/Navbar.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { 
+    AppBar,
+    Toolbar,
+    Typography
+} from '@mui/material';
+
+const Navbar = ()=> {
+    return (
+        <>
+            <AppBar>
+                <Toolbar>
+                    <Typography variant='h4'>Logo</Typography>
+                    <div className='nav-link-container'>
+                        <Link to='' className='nav-link'>Home</Link>
+                        <Link to='' className='nav-link'>Home</Link>
+                        <Link to='' className='nav-link'>Home</Link>
+                        <Link to='' className='nav-link'>Home</Link>
+                    </div>
+                </Toolbar>
+            </AppBar>
+            
+        </>
+    )
+}
+
+export default Navbar
