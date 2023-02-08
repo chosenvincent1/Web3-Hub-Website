8d0eae3bc16739da8ba172f2f32886f06760a6e0
Chosen Vincent <chosendev2000@gmail.com>
2023-02-08 12:45:00 -0800

    changing Navbar and other component

--- src/Components/Team.js
+++ src/Components/Team.js
@@ -6,25 +6,32 @@ const Team = ()=> {
         <div className="team">
             <div className="team-heading-container">
                 <h1 className="team-heading-title">Our Team</h1>
+                <h2>Meet our awesome team of instructors</h2>
             </div>
             <div className="team-container">
                 <div className="each-team-container">
-
+                    <img src="../Images/Member.svg" alt="Web Developer"/>
+                    <h3>Web Developer</h3>
                 </div>
                 <div className="each-team-container">
-
+                    <img src="../Images/Member.svg" alt="Web Developer"/>
+                    <h3>Web Developer</h3>
                 </div>
                 <div className="each-team-container">
-
+                    <img src="../Images/Member.svg" alt="Web Developer"/>
+                    <h3>Web Developer</h3>
                 </div>
                 <div className="each-team-container">
-
+                    <img src="../Images/Member.svg" alt="Web Developer"/>
+                    <h3>Web Developer</h3>
                 </div>
                 <div className="each-team-container">
-
+                    <img src="../Images/Member.svg" alt="Web Developer"/>
+                    <h3>Web Developer</h3>
                 </div>
                 <div className="each-team-container">
-
+                    <img src="../Images/Member.svg" alt="Web Developer"/>
+                    <h3>Web Developer</h3>
                 </div>
             </div>
         </div>
