01f2e19a4de9e40aae6135b96848075f9e68c06a
Chosen <chosendev2000@gmail.com>
2023-03-14 16:07:20 +0100

    made some changes

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+// import { useSpring, animated } from 'react-spring';
 
 import Navbar from '../Components/Navbar';
 import Team from '../Components/Team';
@@ -11,14 +12,25 @@ import OurMission from '../Components/OurMission';
 import WhyChooseUs from '../Components/WhyChooseUs';
 import '../Styles/Home.css';
 
-import bitcoin from '../Images/Bitcoin.svg';
-import dollar from '../Images/Dollar.svg';
-import ethereum from '../Images/Ethereum.svg';
-import tether from '../Images/Tether.svg';
+// import bitcoin from '../Images/Bitcoin.svg';
+// import dollar from '../Images/Dollar.svg';
+// import ethereum from '../Images/Ethereum.svg';
+// import tether from '../Images/Tether.svg';
 
 
 const Home = ()=> {
 
+    // function getRandomPosition() {
+    //   const x = Math.random() * window.innerWidth;
+    //   const y = Math.random() * window.innerHeight;
+    //   return { x, y };
+    // }
+
+    // const styles = useSpring({
+    //   to: getRandomPosition(),
+    //   from: getRandomPosition(),
+    //   config: { tension: 200, friction: 100 },
+    // });
       
     return (
         <>
@@ -28,10 +40,10 @@ const Home = ()=> {
                 <p className='home-md-text'>welcome to web3 hub</p>
                 <h1 className='home-lg-text'>Unlocking The Potential Of The African Blockchain Ecosystem.</h1>
                 <p className='home-sm-text'>DREAM IT, THINK IT, AND DO IT.</p>
-                <img src={bitcoin} alt='Bitcoin' className='coin-image bitcoin' />
+                {/* <img src={bitcoin} alt='Bitcoin' className='coin-image bitcoin' />
                 <img src={tether} alt='Tether' className='coin-image tether' />
-                <img src={ethereum} alt='Ethereum' className='coin-image ethereum'/>
-                <img src={dollar} alt='Dollar' className='coin-image dollar' />
+                <img src={ethereum} alt='Ethereum' className='coin-image ethereum' />
+                <img src={dollar} alt='Dollar' className='coin-image dollar' /> */}
             </div>
         </div>
         <About />
